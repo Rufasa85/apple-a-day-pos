41bb5348d40de21becda5defc4d52785f19e2025
Eli Wood <mreliwood1@gmail.com>
2023-08-09 22:17:49 -0700

    merge

--- src/components/Customer/Card.js
+++ src/components/Customer/Card.js
@@ -43,18 +43,22 @@ const Card = ({ customer, setCustomer }) => {
 	};
 
 	return (
-		<div onClick={() => setVisible(true)} className='p-5 bg-white ring-inset ring-1 ring-gray-200 shadow shadow-gray-200 cursor-pointer h-20 gap-3 rounded-xl w-full flex'>
+		<div onClick={() => setVisible(true)} className='p-5 bg-white ring-inset ring-1 ring-gray-200 shadow shadow-gray-200 cursor-pointer h-20 gap-3 rounded-2xl w-full flex relative'>
 			<div className='ring-2 ring-gray-900 rounded-full aspect-square h-full overflow-hidden justify-center flex'>
 				<Icons.User className='w-12 h-12 fill-gray-900' />
 			</div>
 
-			<div className='justify-center items-center grow flex flex-col'>
+			<div className='h-full justify-center items-center grow flex flex-col'>
 				<div className='sm:text-sm text-base w-full justify-between flex'>
-					<p className='font-semibold'>{firstName}</p>
+					<div className='gap-1 h-fit justify-end items-end flex'>
+						<p className='font-semibold'>{firstName}</p>
+					</div>
 				</div>
 
 				<div className='sm:text-sm text-base w-full justify-between flex'>
-					<p>{lastName}</p>
+					<div className='gap-1 h-fit justify-end items-end flex'>
+						<p className=''>{lastName}</p>
+					</div>
 
 					{dateOfBirth && (
 						<div className='gap-1 h-full justify-end items-end flex'>
@@ -71,6 +75,8 @@ const Card = ({ customer, setCustomer }) => {
 					)}
 				</div>
 			</div>
+
+			<p className='text-gray-400 hover:text-red-600 p-px inset-0 top-full w-full text-center sm:text-sm text:base absolute'>Remove Customer</p>
 
 			<Modal options={modalOptions} />
 		</div>
